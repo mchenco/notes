3e865362db91597381a0d6b1e5f4335301384108
Michelle Chen <chen.michelle15@gmail.com>
2016-12-29 16:59:17 -0500

    finished most sorting algos

--- sorting.py
+++ sorting.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import queue
 
 #O(n^2)
 #bubbles each element to the correct spot
@@ -14,7 +15,10 @@ def bubbleSort(lst):
 	return lst
 
 #O(n^2)
+#stable
 '''
+use when n is guaranteed to be small
+
 if next element is smaller than current
 bubbles it down through list until correct spot
 left side of list will always be sorted
@@ -45,19 +49,20 @@ def selectionSort(lst):
 	return lst
 
 #O(nlogn)
+#stable
 '''
+uses a lot of memory
+
 recursively divides and sorts half of list
 merges the list into a sorted list in the end
 '''
-def mergeSort(lst, first, last):
-	ct = 0
-	if last - first <= 1:
+def mergeSort(lst):
+	if len(lst) <= 1:
 		return lst
-	mid = (first+last)//2
+	mid = len(lst)//2
 
-	a = mergeSort(lst, first, mid)
-	b = mergeSort(lst, mid, last)
-
+	a = mergeSort(lst[:mid])
+	b = mergeSort(lst[mid:])
 
 	return _merge(a, b)
 
@@ -66,25 +71,29 @@ def _merge(a, b):
 	act = 0
 	bct = 0
 
-	for i in range(min(len(a), len(b))):
-		if a[i] < b[i]:
-			mergedlst.append(a[i])
-			act += 1
-		else:
-			mergedlst.append(b[i])
-			bct += 1
-	
-	if len(a) < len(b):
-		while bct <= len(b)-1:
-			mergedlst.append(b[bct])
-			bct += 1
-	else:
-		while act <= len(a)-1:
+	#while still elements to be sorted
+	while act < len(a) and bct < len(b):
+		if a[act] < b[bct]:
 			mergedlst.append(a[act])
 			act += 1
+		else:
+			mergedlst.append(b[bct])
+			bct += 1
+	
+	#if elements leftover in array,
+	#fill sorted array
+	while bct <= len(b)-1:
+		mergedlst.append(b[bct])
+		bct += 1
+
+	while act <= len(a)-1:
+		mergedlst.append(a[act])
+		act += 1
+
 	return mergedlst
 
 #O(nlogn)
+#unstable
 '''
 as lst is more ordered, quicksort is less efficient, worst-case O(n^2)
 Introsort is alternative if this ^ is the case.
@@ -104,8 +113,16 @@ def quickSort(lst):
 
 #O(n+w)
 '''
-uses a counter array to count the elements that are repeated
+w is the range of numbers
+the smaller the range, the better
+works best when w < n ; takes a lot of time/space if w is large
+better than comparison sorts which have lowerbound O(nlogn)
 
+uses a count array to count the elements that are repeated
+modify count array with the preceding sums
+there are count[i] elems preceding the current elem in list
+by placing according to how many preceding elements there are,
+results in a sorted list
 '''
 def countingSort(lst):
 	count = [0] * max(lst)
@@ -123,8 +140,37 @@ def countingSort(lst):
 		count[lst[i]] -= 1
 	return sorted
 
+#O(kn)
+#stable
+'''
+k is number of digits
+use when logn is much larger than k OR 
+use when the longest integer is shorter than logn
+if k is logn —— O(nlogn), then you should just use quickSort/countingSort
 
-# def radixSort(lst):
+linear time because we traverse the dataset k (# of digits) times
+as the input grows, the amount of time needed grows linearly
+
+sort based on sigfigs
+empty buckets into a 'ordered' list
+repeat for max #of digits
+'''
+def radixSort(lst):
+	#find number with most digits x
+	digits = len(str(max(lst)))
+	buckets = [queue.Queue() for x in range(10)] #array of queues
+	#sort by last digits into buckets
+	for i in range(digits):
+		for num in lst:
+			digit = (num // 10**i) % 10
+			buckets[digit].put(num)
+
+		#empty buckets in order of a queue
+		lst = []
+		for bucket in buckets:
+			while not bucket.empty():
+				lst.append(bucket.get())
+	return lst
 
 def _swap(lst, indexA, indexB):
 	temp = lst[indexA]
@@ -133,7 +179,7 @@ def _swap(lst, indexA, indexB):
 
 '''
 takes CLA as input for testing or supplies default list
-musy specify what sorting algorithm to use as argv[2]
+must specify what sorting algorithm to use as argv[2]
 
 `python3 sorting.py bubble 9 1 3 48 123`
 '''
@@ -150,11 +196,13 @@ def main(argv):
 	if sys.argv[1]  == "selection":
 		print("selection sort: ", selectionSort(lst))
 	if sys.argv[1]  == "merge":
-		print("merge sort: ", mergeSort(lst, 0, len(lst)))
+		print("merge sort: ", mergeSort(lst))
 	if sys.argv[1]  == "quick":
 		print("quick sort: ", quickSort(lst))
 	if sys.argv[1] == "counting":
 		print("counting sort: ", countingSort(lst))
+	if sys.argv[1] == "radix":
+		print("radix sort: ", radixSort(lst))
 
 if __name__ == "__main__":
 	main(sys.argv)
